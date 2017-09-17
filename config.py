b62825ce73f50955b9b8dfc9db0fed82b28a27fb
luanzun <fuyun365@gmail.com>
2017-09-17 08:01:30 +0800

    update

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# dialect+driver://username:password@host:port/database?charset=utf8
-# 配置 sqlalchemy  数据库驱动://数据库用户名:密码@主机地址:端口/数据库?编码
-SQLALCHEMY_DATABASE_URI = 'mysql://pyte.vip:pyte.vip@139.199.6.52:3306/pyte.vip?charset=utf8'
-#设置这一项是每次请求结束后都会自动提交数据库中的变动
-
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 #!flask/bin/python3
+# -*- conding: utf-8 -*-
 from app import app
 app.run(debug = True)
